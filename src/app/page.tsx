ec1950f0d636924201aa70e86e3f34e4d1bbfc53
Sergio Sanchez <ssanchezfilho@gmail.com>
2023-08-02 12:21:47 -0300

    feat: fix components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ interface MovieCardProps {
 
 const apiKey = process.env.NEXT_PUBLIC_API_KEY
 
-export function MovieCard({movie}: MovieCardProps) {
+function MovieCard({movie}: MovieCardProps) {
   const [movieDetails, setMovieDetails] = useState<Movie["movieDetails"]>([])
   const [movieRunTime, setMovieRuntime] = useState(0)
   const [movieVideo, setMovieVideo] = useState<Movie["movieVideo"]>([])
@@ -121,7 +121,7 @@ export function MovieCard({movie}: MovieCardProps) {
   )
 }
 
-export function EmptyMovieCard() {
+function EmptyMovieCard() {
   return (
     <div className='flex flex-col sm:flex-row gap-9 items-center animate-pulse cursor-wait'>
       <div className='flex flex-col gap-3'>
